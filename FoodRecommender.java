083b3d51eeb59d7fa71d0e641ff5b902195cb64a
Aneesh Dake <adake@purdue.edu>
2025-02-04 16:32:39 -0500

    Create FoodRecommender.java

---
+++ FoodRecommender.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class FoodRecommender {
+
+    public static final String WELCOME_MESSAGE = "Welcome to the Food Recommender!";
+    public static final String INITIAL_FOOD = "Do you want to have good food?";
+    public static final String SPICY = "Do you like spicy food?";
+    public static final String CHINESE = "Do you want to try spicy Chinese Food?";
+    public static final String INDIAN = "Do you want to try spicy Indian Food?";
+    public static final String ETHIOPIAN = "Do you want to try spicy Ethiopian food?";
+    public static final String SEA_FOOD = "Do you like sea food?";
+    public static final String SUSHI = "Do you like traditional sushi?";
+    public static final String BEEF = "Do you like beef?";
+    public static final String PORK = "Do you like pork?";
+    public static final String LAMB = "Do you like lamb?";
+    public static final String SOUP = "Do you want soup with beef?";
+    public static final String GOODBYE_MESSAGE = "Thank you for using" +
+            " the Food Recommender!";
+
+
+    public static final String HOT_POT = "Sichuan hot pot is a great choice!";
+    public static final String VINDALOO = "Vindaloo is a great choice!";
+    public static final String DORO_WAT = "Doro wat is very good!";
+    public static final String PENNE = "Penne all’arrabbiata is delicious!";
+    public static final String SALMON_SUSHI = "Salmon sushi will fulfill you!";
+    public static final String BBQ_SUSHI = "BBQ bacon sushi is a great combination of sushi and meat!";
+    public static final String FRUIT_SALAD = "Sweet fruit salad is a great choice!";
+    public static final String PORK_QUESADILLAS = "Pork quesadillas are very tasty!";
+    public static final String LAMB_CHOPS = "Rosemary roast lamb chops are a great choice!";
+    public static final String BIBIMBAP = "Beef and veggie bibimbap will fulfill you!";
+    public static final String PHO_SOUP = "Vietnamese beef and noodle pho soup is very good!";
+    // ------------------------- DO NOT MODIFY ABOVE -------------------------
+
+    // IMPLEMENT YOUR SOLUTION HERE
+   
+}
